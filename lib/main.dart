e000c258ea193971fa2ac897a196030ebf29bcde
Leo276234109 <44420902+Leo276234109@users.noreply.github.com>
2020-08-13 23:36:10 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -43,15 +31,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -66,11 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     print("_incrementCounter _incrementCounter");
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
       FToast.toast(context,msg: "counter $_counter",msgStyle: TextStyle(color: Colors.white),);
     });
@@ -78,11 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-//    if(Platform.isIOS){
-//      OrientationPlugin.setPreferredOrientations([DeviceOrientation.landscapeRight]);
-//    }
-//
-//    OrientationPlugin.forceOrientation(DeviceOrientation.landscapeRight);
     super.initState();
   }
   iosShowTop() async{
@@ -107,10 +76,6 @@ class _MyHomePageState extends State<MyHomePage> {
  }
   @override
   Widget build(BuildContext context) {
-//      _timer?.cancel();
-//      _timer = Timer.periodic(_timeoutSeconds, (_){
-//        iosShowTop();
-//      });
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -126,14 +91,12 @@ class _MyHomePageState extends State<MyHomePage> {
             },
           ),
       Material(
-        //带给我们Material的美丽风格美滋滋。你也多看看这个布局
         elevation: 1,
         color: Colors.transparent,
         shape: const StadiumBorder(),
         child: InkWell(
           borderRadius: BorderRadius.all(Radius.circular(50)),
           onTap: _incrementCounter,
-          //来个飞溅美滋滋。
           splashColor:  Colors.amber,
           child: Ink(
             width: 200,

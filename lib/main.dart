3eef7d793014f3b7c4f1efff1d21e1e92f29cb2e
pengshuiming <276234109@qq.com>
2020-08-13 20:53:27 +0800

    readme

--- lib/main.dart
+++ lib/main.dart
@@ -64,13 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
   Timer _timer;
   Duration _timeoutSeconds = const Duration(seconds: 1);
   void _incrementCounter() {
+    print("_incrementCounter _incrementCounter");
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      print("_incrementCounter _incrementCounter");
       _counter++;
       FToast.toast(context,msg: "counter $_counter",msgStyle: TextStyle(color: Colors.white),);
     });
